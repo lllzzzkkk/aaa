cd607301c4f6020d51a8bb4f6f764d3580055a3a
lzk <1461747142@qq.com>
2018-08-25 10:37:16 +0800

    aa

---
+++ src/main/java/com/exampleq/demoq/DemoqApplication.java
@@ -0,0 +1,12 @@
+package com.exampleq.demoq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoqApplication.class, args);
+	}
+}
